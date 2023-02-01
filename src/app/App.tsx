71a6c38528c169530e7184f3613928a5858b0aec
keka3avr <kekazavr228@gmail.com>
2023-02-01 18:31:47 +1100

    first setup react grid

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
+import { Column } from '@devexpress/dx-react-grid';
 import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui';
+import { generateRows } from './utils/generator';
 
-const columns = [
-    { name: 'id', title: 'ID' },
-    { name: 'product', title: 'Product' },
-    { name: 'owner', title: 'Owner' },
-];
-const rows = [
-    { id: 0, product: 'DevExtreme', owner: 'DevExpress' },
-    { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' },
-];
-
-export default () => (
-    <Paper>
-        <Grid rows={rows} columns={columns}>
-            <Table />
-            <TableHeaderRow />
-        </Grid>
-    </Paper>
-);
+export default function App() {
+    const rows = [
+        {
+            flight: '31_Асача',
+            receipt: '09/50',
+            loadDate: '03.12.2022',
+            boat: 'СРТМ Си-Хантер',
+            consignee: 'ТРК',
+            product: 'Филе сельди',
+            sort: '',
+            package: 22.5,
+            placeAmount: 374,
+            productAmountKg: 8415,
+            operation: 'inner market',
+        },
+    ];
+    const columns = [
+        { name: 'flight', title: '№ Рейса' },
+        { name: 'receipt', title: 'Коносамент' },
+        { name: 'loadDate', title: 'Дата перегруза' },
+        { name: 'boat', title: 'Судно' },
+        { name: 'consignee', title: 'Транспорт' },
+        { name: 'product', title: 'Продукция' },
+        { name: 'sort', title: 'Сорт' },
+        { name: 'package', title: 'Упаковка' },
+        { name: 'placeAmount', title: 'Кол-во мест' },
+        { name: 'productAmountKg', title: 'Объем, кг' },
+        { name: 'operation', title: 'Вид операции' },
+    ];
+    return (
+        <Paper>
+            <Grid />
+        </Paper>
+    );
+}
